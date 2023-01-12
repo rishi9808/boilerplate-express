9eed1868f861c9427a6fc66cebf57db06d8af101
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-01-12 23:31:23 +0530

    Implemented a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -6,6 +6,10 @@ let app = express();
 const path = require('path')
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
+app.use((req,res,next)=>{
+    console.log(req.method+" "+req.path+" - "+req.ip);
+})
+
 // console.log("Hello World");
 
 // app.get("/",((_req,res) =>{
