8c0ff44358759de977c08f08e758c7385e394ee5
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-01-12 15:03:53 +0530

    served an html file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
 
-app.get("/",((_req,res) =>{
-    res.send("Hello Express");
-}))
+// app.get("/",((_req,res) =>{
+//     res.send("Hello Express");
+// }))
+const absolutePath = __dirname + '/views/index.html'
+app.get('/',(req,res)=>{
+    res.sendFile(absolutePath);
+})
 
 
 
